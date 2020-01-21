59ea83fc4a15e47dbaacda443c1f676612ca1ec5
Charlie Parsons <cdaltonparsons@gmail.com>
2020-01-21 12:22:49 -0700

    50% through a quick angular refresher before starting with ionic

---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+
+export class PersonInputComponent {
+  enteredPersonName = '';
+  @Output() personCreate = new EventEmitter<string>();
+
+
+  onCreatePerson() {
+    console.log('Created a person: ' + this.enteredPersonName + '!');
+    this.personCreate.emit(this.enteredPersonName);
+    this.enteredPersonName = '';
+  }
+}
